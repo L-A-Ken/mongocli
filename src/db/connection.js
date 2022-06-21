9ef34d916133e2c870b43370e5ce81ea5a53cb9c
Laura Kenyon <laurakenyon@CN0035.home>
2022-06-21 15:07:02 +0100

    connection made

---
+++ src/db/connection.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+const connection = async () => {
+    try {
+        await client.connect();
+        console.log("success")
+        const db = client.db("Films");
+        return db.collection("Films");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// use this functon to check connection to db connection();
+
+module.exports = { connection, client };
